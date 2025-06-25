d8dcb7c4889b44f4b7de487d9cb75c834134a340
buoysophit <bouysophit11@gmail.com>
2025-06-26 01:38:13 +0700

    phone

--- src/components/iphone/android.tsx
+++ src/components/iphone/android.tsx
@@ -2,11 +2,11 @@ import Android from "@/components/magicui/android";
 
 export function AndroidDemo() {
   return (
-    <div className="relative w-[200px] h-[410px] flex items-center justify-center mx-auto">
-         <Android
-           className="size-full"
-           videoSrc="assets/video/pro1.mp4"
-         />
-       </div>
+    <div className="relative w-full max-w-xs aspect-[200/410] flex items-center justify-center mx-auto sm:max-w-sm md:max-w-md ml-11 mr-4">
+      <Android
+      className="w-full h-full"
+      videoSrc="assets/video/pro1.mp4"
+      />
+    </div>
   );
 }
